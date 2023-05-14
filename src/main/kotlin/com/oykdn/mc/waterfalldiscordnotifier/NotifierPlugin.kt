7d2f0e63697a138954eb6c9596e96b5170f9a3a7
Oyakodon <oykdn@oykdn.com>
2023-05-14 20:13:13 +0900

    設定用コマンドを追加

--- src/main/kotlin/com/oykdn/mc/waterfalldiscordnotifier/NotifierPlugin.kt
+++ src/main/kotlin/com/oykdn/mc/waterfalldiscordnotifier/NotifierPlugin.kt
@@ -1,14 +1,11 @@
 package com.oykdn.mc.waterfalldiscordnotifier
 
 import com.oykdn.mc.waterfalldiscordnotifier.channel.discord.DiscordChannel
-import com.oykdn.mc.waterfalldiscordnotifier.config.Config
+import com.oykdn.mc.waterfalldiscordnotifier.command.NotifierCommand
+import com.oykdn.mc.waterfalldiscordnotifier.config.ConfigLoader
 import com.oykdn.mc.waterfalldiscordnotifier.listener.BungeePlayerEventListener
 import com.oykdn.mc.waterfalldiscordnotifier.listener.PlayerEventNotifier
 import net.md_5.bungee.api.plugin.Plugin
-import net.md_5.bungee.config.Configuration
-import net.md_5.bungee.config.ConfigurationProvider
-import net.md_5.bungee.config.YamlConfiguration
-import java.io.File
 
 
 @Suppress("unused")
@@ -20,44 +17,18 @@ class NotifierPlugin : Plugin() {
     override fun onEnable() {
         logger.info("loading...")
 
-        val config = loadConfig()
+
+        val config = ConfigLoader(dataFolder, CONFIG_PATH)
 
         val notifier = PlayerEventNotifier(
             config, listOf(DiscordChannel(config))
         )
-        proxy.pluginManager.registerListener(
-            this, BungeePlayerEventListener(notifier)
-        )
+
+        proxy.pluginManager.let {
+            it.registerListener(this, BungeePlayerEventListener(notifier))
+            it.registerCommand(this, NotifierCommand(config, description))
+        }
 
         logger.info("initialized.")
-    }
-
-    /**
-     * 設定ファイルを読み込む。
-     * 設定ファイルがない場合はデフォルト値で新規作成する。
-     */
-    private fun loadConfig(): Config {
-        val yamlConfig: Configuration = runCatching {
-            ConfigurationProvider.getProvider(YamlConfiguration::class.java).load(
-                File(
-                    dataFolder, CONFIG_PATH
-                )
-            )
-        }.fold(onSuccess = { it }, onFailure = {
-            if (!dataFolder.exists()) {
-                dataFolder.mkdir()
-            }
-
-            val configuration = Config.default()
-            ConfigurationProvider.getProvider(YamlConfiguration::class.java).save(
-                configuration, File(
-                    dataFolder, CONFIG_PATH
-                )
-            )
-
-            configuration
-        })
-
-        return Config.parse(yamlConfig)
     }
 }
