e27fce6a6199b6af09071c5e6585a7d1d22abe5e
Oyakodon <oykdn@oykdn.com>
2023-05-14 17:06:48 +0900

    Discord通知を実装

---
+++ src/main/kotlin/com/oykdn/mc/waterfalldiscordnotifier/channel/discord/Discord.kt
@@ -0,0 +1,69 @@
+package com.oykdn.mc.waterfalldiscordnotifier.channel.discord
+
+import com.oykdn.mc.waterfalldiscordnotifier.channel.Channel
+import com.oykdn.mc.waterfalldiscordnotifier.config.Config
+import com.oykdn.mc.waterfalldiscordnotifier.model.DiscordWebhookPayload
+import com.oykdn.mc.waterfalldiscordnotifier.model.Notification
+import net.md_5.bungee.api.ProxyServer
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+abstract class Discord(
+    private val config: Config,
+) : Channel {
+    companion object {
+        private const val BASE_URL = "https://discord.com/api/webhooks/"
+    }
+
+    abstract override fun send(n: Notification)
+
+    private val logger = ProxyServer.getInstance().logger
+
+    /**
+     * okHttpクライアント
+     * デバッグモードの際はボディもログ出力する。
+     */
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor()
+                .setLevel(if (config.isDebug) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE)
+        )
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun post(message: DiscordWebhookPayload) {
+        // 必要な設定がない場合は何もしない
+        if (config.discordWebhookId.isEmpty() || config.discordWebhookToken.isEmpty()) {
+            logger.warning("discord: webhook id/token are invalid.")
+            return
+        }
+
+        // POST処理
+        // エラーの場合はログに出力
+        runCatching {
+            retrofit
+                .create(DiscordWebhookService::class.java)
+                .post(
+                    config.discordWebhookId,
+                    config.discordWebhookToken,
+                    message
+                ).execute()
+        }.apply {
+            onSuccess {
+                if (!it.isSuccessful) {
+                    logger.warning("discord: failed to post with code=${it.code()}")
+                }
+            }
+            onFailure {
+                logger.warning("discord: " + it.message)
+            }
+        }
+    }
+}
