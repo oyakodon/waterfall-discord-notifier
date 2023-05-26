ff5ece471789cb1395b4f3c9976e958304aa6cfc
Oyakodon <oykdn@oykdn.com>
2023-05-27 02:33:08 +0900

    fix: 設定に種別ごとのハイライト色を追加

--- src/main/kotlin/com/oykdn/mc/waterfalldiscordnotifier/config/Config.kt
+++ src/main/kotlin/com/oykdn/mc/waterfalldiscordnotifier/config/Config.kt
@@ -7,19 +7,25 @@ data class Config(
     var isDebug: Boolean = false,
     val discordWebhookId: String = "",
     val discordWebhookToken: String = "",
+    val colorJoined: Int = 0,
     val templateJoined: String = "",
+    val colorSwitched: Int = 0,
     val templateSwitched: String = "",
+    val colorLeft: Int = 0,
     val templateLeft: String = "",
 
     ) {
     companion object {
+        private const val KEY_ENABLED = "enabled"
         private const val KEY_DEBUG = "debug"
         private const val KEY_DISCORD_WEBHOOK_ID = "discord.webhookId"
         private const val KEY_DISCORD_WEBHOOK_TOKEN = "discord.webhookToken"
-        private const val KEY_TEMPLATE_JOINED = "template.joined"
-        private const val KEY_TEMPLATE_SWITCHED = "template.switched"
-        private const val KEY_TEMPLATE_LEFT = "template.left"
-        private const val KEY_ENABLED = "enabled"
+        private const val KEY_MSG_JOINED_COLOR = "message.joined.color"
+        private const val KEY_MSG_JOINED_TEMPLATE = "message.joined.template"
+        private const val KEY_MSG_SWITCHED_COLOR = "message.switched.color"
+        private const val KEY_MSG_SWITCHED_TEMPLATE = "message.switched.template"
+        private const val KEY_MSG_LEFT_COLOR = "message.left.color"
+        private const val KEY_MSG_LEFT_TEMPLATE = "message.left.template"
 
         fun load(conf: Configuration): Config {
             return Config(
@@ -27,9 +33,12 @@ data class Config(
                 isDebug = conf.getBoolean(KEY_DEBUG, false),
                 discordWebhookId = conf.getString(KEY_DISCORD_WEBHOOK_ID, ""),
                 discordWebhookToken = conf.getString(KEY_DISCORD_WEBHOOK_TOKEN, ""),
-                templateJoined = conf.getString(KEY_TEMPLATE_JOINED, ""),
-                templateSwitched = conf.getString(KEY_TEMPLATE_SWITCHED, ""),
-                templateLeft = conf.getString(KEY_TEMPLATE_LEFT, ""),
+                colorJoined = conf.getInt(KEY_MSG_JOINED_COLOR, 0),
+                templateJoined = conf.getString(KEY_MSG_JOINED_TEMPLATE, ""),
+                colorSwitched = conf.getInt(KEY_MSG_SWITCHED_COLOR, 0),
+                templateSwitched = conf.getString(KEY_MSG_SWITCHED_TEMPLATE, ""),
+                colorLeft = conf.getInt(KEY_MSG_LEFT_COLOR, 0),
+                templateLeft = conf.getString(KEY_MSG_LEFT_TEMPLATE, ""),
             )
         }
     }
@@ -39,8 +48,11 @@ data class Config(
         set(KEY_DEBUG, isDebug)
         set(KEY_DISCORD_WEBHOOK_ID, discordWebhookId)
         set(KEY_DISCORD_WEBHOOK_TOKEN, discordWebhookToken)
-        set(KEY_TEMPLATE_JOINED, templateJoined)
-        set(KEY_TEMPLATE_SWITCHED, templateSwitched)
-        set(KEY_TEMPLATE_LEFT, templateLeft)
+        set(KEY_MSG_JOINED_COLOR, colorJoined)
+        set(KEY_MSG_JOINED_TEMPLATE, templateJoined)
+        set(KEY_MSG_SWITCHED_COLOR, colorSwitched)
+        set(KEY_MSG_SWITCHED_TEMPLATE, templateSwitched)
+        set(KEY_MSG_LEFT_COLOR, colorLeft)
+        set(KEY_MSG_LEFT_TEMPLATE, templateLeft)
     }
 }
