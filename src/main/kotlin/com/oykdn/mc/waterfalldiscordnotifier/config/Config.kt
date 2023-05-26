16a293a47d63afa8edf4017ee94d8b42c11a3970
Oyakodon <oykdn@oykdn.com>
2023-05-27 02:17:58 +0900

    feat: メッセージテンプレートを設定に追加

--- src/main/kotlin/com/oykdn/mc/waterfalldiscordnotifier/config/Config.kt
+++ src/main/kotlin/com/oykdn/mc/waterfalldiscordnotifier/config/Config.kt
@@ -7,11 +7,18 @@ data class Config(
     var isDebug: Boolean = false,
     val discordWebhookId: String = "",
     val discordWebhookToken: String = "",
-) {
+    val templateJoined: String = "",
+    val templateSwitched: String = "",
+    val templateLeft: String = "",
+
+    ) {
     companion object {
         private const val KEY_DEBUG = "debug"
         private const val KEY_DISCORD_WEBHOOK_ID = "discord.webhookId"
         private const val KEY_DISCORD_WEBHOOK_TOKEN = "discord.webhookToken"
+        private const val KEY_TEMPLATE_JOINED = "template.joined"
+        private const val KEY_TEMPLATE_SWITCHED = "template.switched"
+        private const val KEY_TEMPLATE_LEFT = "template.left"
         private const val KEY_ENABLED = "enabled"
 
         fun load(conf: Configuration): Config {
@@ -20,6 +27,9 @@ data class Config(
                 isDebug = conf.getBoolean(KEY_DEBUG, false),
                 discordWebhookId = conf.getString(KEY_DISCORD_WEBHOOK_ID, ""),
                 discordWebhookToken = conf.getString(KEY_DISCORD_WEBHOOK_TOKEN, ""),
+                templateJoined = conf.getString(KEY_TEMPLATE_JOINED, ""),
+                templateSwitched = conf.getString(KEY_TEMPLATE_SWITCHED, ""),
+                templateLeft = conf.getString(KEY_TEMPLATE_LEFT, ""),
             )
         }
     }
@@ -29,5 +39,8 @@ data class Config(
         set(KEY_DEBUG, isDebug)
         set(KEY_DISCORD_WEBHOOK_ID, discordWebhookId)
         set(KEY_DISCORD_WEBHOOK_TOKEN, discordWebhookToken)
+        set(KEY_TEMPLATE_JOINED, templateJoined)
+        set(KEY_TEMPLATE_SWITCHED, templateSwitched)
+        set(KEY_TEMPLATE_LEFT, templateLeft)
     }
 }
